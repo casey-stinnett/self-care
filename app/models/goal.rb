cb326a72a7746598e4f92211d6231b160277fe83
Casey Stinnett <stinnett.casey@gmail.com>
2018-03-27 17:55:15 -0600

    fixed seed file to show example, got icons working

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,7 @@
 class Goal < ApplicationRecord
   belongs_to :user
+
+  def self.icon_list
+    ['palette', 'hot_tub', 'hotel', 'call', 'local_drink', 'restaurant', 'phonelink_erase', 'fitness_center', 'wb_sunny', 'directions_walk', 'create', 'spa', 'music_note', 'pets', 'import_contacts', 'group']
+  end
 end
