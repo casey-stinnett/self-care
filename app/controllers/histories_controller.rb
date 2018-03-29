d3f94653b9e1d17fbf99bccab32ba97f9c3d94b2
Amber Blackwell <ambereblackwell@gmail.com>
2018-03-29 16:21:32 -0600

    styled goals partial. started debugging addition of goals to welcome modal.

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,22 +1,31 @@
 class HistoriesController < ApplicationController
   def index
+    @histories = Histories.all
   end
 
   def new
+    @history = History.new
   end
 
   def create
+    @history = current_user.history.create(params[:id])
   end
 
   def show
+    @history = History.find(params[:id])
   end
 
   def edit
+    @history = current_user.find(params[:id])
   end
 
   def update
+    @history = current_user.history.find(params[:id])
+    @history.update_attributes(history_params)
   end
 
   def destroy
+    current_user.histories.find(params[:id]).destroy
   end
+
 end
